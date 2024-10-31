58ba52f3e68d705be4e92bd5b6e0c4a75338f85b
Ennard-1 <caiohbn456@gmail.com>
2024-10-31 16:13:43 -0400

    Reapply "getConteudosByUsuario"
    This reverts commit 746b563502863caaaab9f1004721eef79deeec5c.

--- obj/Debug/net8.0/StreamingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StreamingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StreamingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfeb3c4bd92efe3ff0eef888d55533151ac02a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06df5fbbf820e8cc9129bdd8a2a8c353a530c4b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StreamingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StreamingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
